3efc7087b47102c248211c2976f784d741d8057b
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-29 12:33:18 +0100

    Added comments to explain my solution Solved the problem using nested loops

--- main.rb
+++ main.rb
@@ -5,11 +5,18 @@
 # => [1,4]  # for a profit of $15 - $3 == $12
 
 def stock_picker(prices)
-  prices.each_with_index do |numi, i|
-    prices[i+1..-1].each do |numj|
-      puts "#{numi}, #{numj}"
+  difmax = 0    #variable to store max profit
+  best_days = []    #variable to store best days to buy and sell
+  prices.each_with_index do |numi, i|   #iterate thru all prices
+    prices[i+1..-1].each_with_index do |numj, j|    #keep track of new sliced array
+      dif = numj - numi   #variable to store current dif (profit)
+      if dif > difmax
+        difmax = dif
+        best_days = [i, i+1+j]
+      end
     end
   end
+  puts "max profit is: #{difmax}, best day: #{best_days}"
 end
 
 stock_prices = [17,3,6,9,15,8,6,1,10]
