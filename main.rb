525b36f49f8ba3f20f39d188d86d68f955622de6
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-29 12:37:57 +0100

    Added a more elegant solution as comment

--- main.rb
+++ main.rb
@@ -4,6 +4,25 @@
 # > stock_picker([17,3,6,9,15,8,6,1,10])
 # => [1,4]  # for a profit of $15 - $3 == $12
 
+# a more elegant solution:
+# def stock_picker(prices)
+#   min_price = prices.first
+#   min_day = 0
+#   max_profit = 0
+#   best_days = []
+
+#   prices.each_with_index do |price, day|
+#     if price < min_price
+#       min_price = price
+#       min_day = day
+#     elsif price - min_price > max_profit
+#       max_profit = price - min_price
+#       best_days = [min_day, day]
+#     end
+#   end
+#   best_days
+# end
+
 def stock_picker(prices)
   difmax = 0    #variable to store max profit
   best_days = []    #variable to store best days to buy and sell
