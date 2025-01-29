d5e5fb39ae74023ea70bf31985c732ad62655276
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-29 11:41:22 +0100

    Added main comment to explain code problem

--- main.rb
+++ main.rb
@@ -1,5 +1,22 @@
+# Implement a method #stock_picker that takes in an array of stock prices, 
+# one for each hypothetical day. It should return a pair of days representing 
+# the best day to buy and the best day to sell. Days start at 0.
+# > stock_picker([17,3,6,9,15,8,6,1,10])
+# => [1,4]  # for a profit of $15 - $3 == $12
+
 def stock_picker(prices)
-  puts "something"
+  # prices.each do |i|
+  #   prices.each do |j|
+  #     print i, j, " "
+  #   end
+  #   print "\n"
+  # end
+  for i in prices
+    for j in prices
+      print i, "-", j, " "
+    end
+    print "\n"
+  end
 end
 
 stock_prices = [17,3,6,9,15,8,6,1,10]
